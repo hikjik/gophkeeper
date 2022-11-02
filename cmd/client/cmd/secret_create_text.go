0d2c7d23c848522308ae15ef63fc8d7cf8a53d62
hikjik <hikjik@yandex.ru>
2022-11-02 05:08:56 +0300

    Add secret CRUD cmd

---
+++ cmd/client/cmd/secret_create_text.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"github.com/go-developer-ya-practicum/gophkeeper/internal/client/secret"
+	pb "github.com/go-developer-ya-practicum/gophkeeper/internal/proto"
+)
+
+var createTextSecretCmd = &cobra.Command{
+	Use:   "text",
+	Short: "Create text secret",
+	Run: func(cmd *cobra.Command, args []string) {
+		name, err := cmd.Flags().GetString("name")
+		if err != nil {
+			log.Fatal().Msgf("Error reading secret name: %v", err)
+			return
+		}
+
+		data, err := cmd.Flags().GetString("data")
+		if err != nil {
+			log.Fatal().Msgf("Error reading text data: %v", err)
+			return
+		}
+
+		text := secret.Text{
+			Data: data,
+		}
+
+		content, err := encryptSecret(text)
+		if err != nil {
+			log.Fatal().Msgf("Failed to encrypt secret: %v", err)
+			return
+		}
+
+		resp, err := secretClient.CreateSecret(context.Background(), &pb.CreateSecretRequest{
+			Name:    name,
+			Content: content,
+		})
+		if err != nil {
+			log.Fatal().Msgf("Failed to create secret: %v", err)
+			return
+		}
+
+		fmt.Printf("Secret %s version %v created successfully\n", resp.GetName(), resp.GetVersion())
+	},
+}
+
+func init() {
+	createSecretCmd.AddCommand(createTextSecretCmd)
+
+	createTextSecretCmd.Flags().String("name", "", "Secret name")
+	if err := createTextSecretCmd.MarkFlagRequired("name"); err != nil {
+		log.Error().Err(err)
+	}
+	createTextSecretCmd.Flags().String("data", "", "Text data")
+	if err := createTextSecretCmd.MarkFlagRequired("data"); err != nil {
+		log.Error().Err(err)
+	}
+}
---
+++ cmd/client/cmd/secret_update_credentials.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/cobra"
+
+	"github.com/go-developer-ya-practicum/gophkeeper/internal/client/secret"
+	pb "github.com/go-developer-ya-practicum/gophkeeper/internal/proto"
+)
+
+var updateCredentialsSecretCmd = &cobra.Command{
+	Use:   "credentials",
+	Short: "Update credentials secret",
+	Run: func(cmd *cobra.Command, args []string) {
+		name, err := cmd.Flags().GetString("name")
+		if err != nil {
+			log.Fatal().Msgf("Error reading secret name: %v", err)
+		}
+
+		login, err := cmd.Flags().GetString("login")
+		if err != nil {
+			log.Fatal().Msgf("Error reading login: %v", err)
+		}
+
+		password, err := cmd.Flags().GetString("password")
+		if err != nil {
+			log.Fatal().Msgf("Error reading password: %v", err)
+		}
+
+		credentials := secret.Credentials{
+			Login:    login,
+			Password: password,
+		}
+
+		content, err := encryptSecret(credentials)
+		if err != nil {
+			log.Fatal().Msgf("Failed to encrypt secret: %v", err)
+			return
+		}
+
+		resp, err := secretClient.UpdateSecret(context.Background(), &pb.UpdateSecretRequest{
+			Name:    name,
+			Content: content,
+		})
+		if err != nil {
+			log.Fatal().Msgf("Failed to update secret: %v", err)
+			return
+		}
+
+		fmt.Printf("Secret %s version %v updated successfully\n", resp.GetName(), resp.GetVersion())
+	},
+}
+
+func init() {
+	updateSecretCmd.AddCommand(updateCredentialsSecretCmd)
+
+	updateCredentialsSecretCmd.Flags().String("name", "", "Secret name")
+	if err := updateCredentialsSecretCmd.MarkFlagRequired("name"); err != nil {
+		log.Error().Err(err)
+	}
+	updateCredentialsSecretCmd.Flags().String("login", "", "Login")
+	if err := updateCredentialsSecretCmd.MarkFlagRequired("login"); err != nil {
+		log.Error().Err(err)
+	}
+	updateCredentialsSecretCmd.Flags().String("password", "", "Password")
+	if err := updateCredentialsSecretCmd.MarkFlagRequired("password"); err != nil {
+		log.Error().Err(err)
+	}
+}
